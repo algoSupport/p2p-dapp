55c78fc36079f1e0167b4eb1c5e55e0cf5f65bfc
algoSupport <hollydev0430@gmail.com>
2023-04-26 18:57:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ import InvoicePrint from "./pages/pre-built/order-invoice/InvoicePrint";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { arbitrum, arbitrumGoerli } from "wagmi/chains";
+import { arbitrumGoerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider } = configureChains(
-  [arbitrum, arbitrumGoerli],
+  [arbitrumGoerli],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 const { connectors } = getDefaultWallets({
