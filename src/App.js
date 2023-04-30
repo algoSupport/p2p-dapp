6b9d2683e7a04300e6f481dc485216999d08ea72
algoSupport <hollydev0430@gmail.com>
2023-04-30 08:16:45 +0200

    update color scheme

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
           }),
           {
             colors: {
-              modalBackground: "#101924",
+              modalBackground: "#0e1721",
             },
           }
         )}
