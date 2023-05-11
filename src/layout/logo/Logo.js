30ff9602cd2aeafe36729b805ffb299a38c97c99
algoSupport <hollydev0430@gmail.com>
2023-05-11 19:00:04 +0200

    add testnet badge

--- src/layout/logo/Logo.js
+++ src/layout/logo/Logo.js
@@ -2,12 +2,16 @@ import React from "react";
 import LogoLight2x from "../../images/logo2x.svg";
 import LogoDark2x from "../../images/logo-dark2x.svg";
 import { Link } from "react-router-dom";
+import { Badge } from "reactstrap";
 
 const Logo = () => {
   return (
     <Link to={`${process.env.PUBLIC_URL}/dashboard`} className="logo-link">
-      <img className="logo-light logo-img h-8" src={LogoLight2x} alt="logo" />
-      <img className="logo-dark logo-img h-8" src={LogoDark2x} alt="logo" />
+      <img className="logo-light logo-img h-8 me-2" src={LogoLight2x} alt="logo" />
+      <img className="logo-dark logo-img h-8 me-2" src={LogoDark2x} alt="logo" />
+      <Badge color="secondary" className="badge-dim">
+        Testnet
+      </Badge>
     </Link>
   );
 };
