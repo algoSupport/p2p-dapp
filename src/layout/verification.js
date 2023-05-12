fa82da8070e19d4b71755edc84b5e5f62f0b1734
algoSupport <hollydev0430@gmail.com>
2023-05-12 07:02:06 -0700

    update verify page

--- src/layout/verification.js
+++ src/layout/verification.js
@@ -1,30 +1,39 @@
 import React, { useState } from "react";
 
+import { Form, Spinner, Alert } from "reactstrap";
 import { useForm } from "react-hook-form";
-import { Col, Form } from "reactstrap";
+import { Link } from "react-router-dom";
+import Head from "./head/Head";
+import PageContainer from "./page-container/PageContainer";
 import {
   Block,
-  BlockBetween,
   BlockContent,
   BlockDes,
   BlockHead,
-  BlockHeadContent,
   BlockTitle,
   Button,
+  Icon,
   PreviewCard,
 } from "../components/Component";
-import Head from "./head/Head";
-import { utils } from "ethers";
+import LogoDark from "../images/logo-dark2x.svg";
+import Logo from "../images/logo2x.svg";
+import AuthFooter from "../pages/auth/AuthFooter";
 import { verify } from "../utils/Utils";
-import { toast } from "react-toastify";
+import { utils } from "ethers";
 
-const Verification = () => {
-  const onFormSubmit = async (submitData) => {
-    if (await verify(submitData.code)) {
-      localStorage.setItem("secret", utils.keccak256(new TextEncoder().encode(submitData.code)));
+const VerifyCode = () => {
+  const [loading, setLoading] = useState(false);
+  const [passState, setPassState] = useState(false);
+  const [errorVal, setError] = useState("");
+
+  const onFormSubmit = async (formData) => {
+    setLoading(true);
+
+    if (await verify(formData.passcode)) {
+      localStorage.setItem("secret", utils.keccak256(new TextEncoder().encode(formData.passcode)));
     } else {
-      // setError("code", { message: "Invalid code" });
-      toast.error("Invalid code");
+      setError("Invalid code");
+      setLoading(false);
     }
   };
 
@@ -32,57 +41,77 @@ const Verification = () => {
 
   return (
     <React.Fragment>
-      <Head title="Verify the code"></Head>
-      <div className="d-flex justify-center align-center flex-column " style={{ width: "100vw", height: "100vh" }}>
-        <div style={{ width: "300px" }}>
-          {/* <BlockHead size="sm">
-            <BlockContent>
-              <BlockHeadContent>
-                <BlockTitle tag="h3" page className="text-center">
-                  Verify the code
-                </BlockTitle>
-                <BlockDes className="text-soft">
-                  <p></p>
+      <Head title="Login" />
+      <PageContainer>
+        <Block className="nk-block-middle nk-auth-body  wide-xs">
+          <div className="brand-logo pb-4 text-center">
+            <Link to={process.env.PUBLIC_URL + "/"} className="logo-link">
+              <img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
+              <img className="logo-dark logo-img logo-img-lg" src={LogoDark} alt="logo-dark" />
+            </Link>
+          </div>
+
+          <PreviewCard className="card-bordered" bodyClass="card-inner-lg">
+            <BlockHead>
+              <BlockContent>
+                <BlockTitle tag="h4">Sign-In</BlockTitle>
+                <BlockDes>
+                  <p>Access Cilistia using your verification code</p>
                 </BlockDes>
-              </BlockHeadContent>
-            </BlockContent>
-          </BlockHead> */}
+              </BlockContent>
+            </BlockHead>
+            {errorVal && (
+              <div className="mb-3">
+                <Alert color="danger" className="alert-icon">
+                  {" "}
+                  <Icon name="alert-circle" /> Invalid code{" "}
+                </Alert>
+              </div>
+            )}
+            <Form className="is-alter" onSubmit={handleSubmit(onFormSubmit)}>
+              <div className="form-group">
+                <div className="form-label-group">
+                  <label className="form-label" htmlFor="password">
+                    Code
+                  </label>
+                </div>
+                <div className="form-control-wrap">
+                  <a
+                    href="#password"
+                    onClick={(ev) => {
+                      ev.preventDefault();
+                      setPassState(!passState);
+                    }}
+                    className={`form-icon lg form-icon-right passcode-switch ${passState ? "is-hidden" : "is-shown"}`}
+                  >
+                    <Icon name="eye" className="passcode-icon icon-show"></Icon>
 
-          <Block size="lg">
-            <PreviewCard>
-              <Form className="row gy-4" noValidate onSubmit={handleSubmit(onFormSubmit)}>
-                <Col md="12">
-                  <div className="form-group">
-                    <BlockTitle tag="h3" page className="text-center mb-3">
-                      Input the verification code
-                    </BlockTitle>
-                    {/* <label className="form-label">Verification Code</label> */}
-                    <input
-                      className="form-control"
-                      type="number"
-                      name="code"
-                      placeholder="code"
-                      ref={register({ required: "This field is required" })}
-                    />
-                    {errors.code && <span className="invalid">{errors.code.message}</span>}
-                  </div>
-                </Col>
-
-                <Col size="12">
-                  <ul className="align-center justify-center flex-wrap flex-sm-nowrap gx-4 gy-2">
-                    <li>
-                      <Button color="primary" type="submit">
-                        Verify
-                      </Button>
-                    </li>
-                  </ul>
-                </Col>
-              </Form>
-            </PreviewCard>
-          </Block>
-        </div>
-      </div>
+                    <Icon name="eye-off" className="passcode-icon icon-hide"></Icon>
+                  </a>
+                  <input
+                    type={passState ? "text" : "password"}
+                    id="password"
+                    name="passcode"
+                    defaultValue="123456"
+                    ref={register({ required: "This field is required" })}
+                    placeholder="Enter your passcode"
+                    onChange={(e) => setError("")}
+                    className={`form-control-lg form-control ${passState ? "is-hidden" : "is-shown"}`}
+                  />
+                  {errors.passcode && <span className="invalid">{errors.passcode.message}</span>}
+                </div>
+              </div>
+              <div className="form-group">
+                <Button size="lg" className="btn-block" type="submit" color="primary">
+                  {loading ? <Spinner size="sm" color="white" /> : "Verify"}
+                </Button>
+              </div>
+            </Form>
+          </PreviewCard>
+        </Block>
+        <AuthFooter />
+      </PageContainer>
     </React.Fragment>
   );
 };
-export default Verification;
+export default VerifyCode;
