bc79f1b6b7bb3d8782ecafb8159fdbadcd254254
algoSupport <hollydev0430@gmail.com>
2023-05-14 08:59:33 +0200

    remove unused

--- src/route/Index.js
+++ src/route/Index.js
@@ -7,66 +7,8 @@ import { RedirectAs404 } from "../utils/Utils";
 import Homepage from "../pages/dashboard/Dashboard";
 import Wallets from "../pages/wallets/Wallets";
 
-import Component from "../pages/components/Index";
-import Accordian from "../pages/components/Accordions";
-import Alerts from "../pages/components/Alerts";
-import Avatar from "../pages/components/Avatar";
-import Badges from "../pages/components/Badges";
-import Breadcrumbs from "../pages/components/Breadcrumbs";
-import ButtonGroup from "../pages/components/ButtonGroup";
-import Buttons from "../pages/components/Buttons";
-import Cards from "../pages/components/Cards";
-import Carousel from "../pages/components/Carousel";
-import Dropdowns from "../pages/components/Dropdowns";
-import FormElements from "../pages/components/forms/FormElements";
-import FormLayouts from "../pages/components/forms/FormLayouts";
-import FormValidation from "../pages/components/forms/FormValidation";
-import CardWidgets from "../pages/components/widgets/CardWidgets";
-import ChartWidgets from "../pages/components/widgets/ChartWidgets";
-import DualListPage from "../pages/components/misc/DualListbox";
-import RatingWidgets from "../pages/components/widgets/RatingWidgets";
-import SlickPage from "../pages/components/misc/Slick";
-import SweetAlertPage from "../pages/components/misc/SweetAlert";
-import BeautifulDnd from "../pages/components/misc/BeautifulDnd";
-import GoogleMapPage from "../pages/components/misc/GoogleMap";
-import Modals from "../pages/components/Modals";
-import Pagination from "../pages/components/Pagination";
-import Popovers from "../pages/components/Popovers";
-import Progress from "../pages/components/Progress";
-import Spinner from "../pages/components/Spinner";
-import Tabs from "../pages/components/Tabs";
-import Toast from "../pages/components/Toast";
-import Tooltips from "../pages/components/Tooltips";
-import Typography from "../pages/components/Typography";
-import CheckboxRadio from "../pages/components/forms/CheckboxRadio";
-import AdvancedControls from "../pages/components/forms/AdvancedControls";
-import InputGroup from "../pages/components/forms/InputGroup";
-import FormUpload from "../pages/components/forms/FormUpload";
-import NumberSpinner from "../pages/components/forms/NumberSpinner";
-import NouiSlider from "../pages/components/forms/nouislider";
-import WizardForm from "../pages/components/forms/WizardForm";
-import UtilBorder from "../pages/components/UtilBorder";
-import UtilColors from "../pages/components/UtilColors";
-import UtilDisplay from "../pages/components/UtilDisplay";
-import UtilEmbeded from "../pages/components/UtilEmbeded";
-import UtilFlex from "../pages/components/UtilFlex";
-import UtilOthers from "../pages/components/UtilOthers";
-import UtilSizing from "../pages/components/UtilSizing";
-import UtilSpacing from "../pages/components/UtilSpacing";
-import UtilText from "../pages/components/UtilText";
-
-import Blank from "../pages/others/Blank";
 import Faq from "../pages/others/Faq";
-import Regularv1 from "../pages/others/Regular-1";
-import Regularv2 from "../pages/others/Regular-2";
 import Terms from "../pages/others/Terms";
-import BasicTable from "../pages/components/table/BasicTable";
-import DataTablePage from "../pages/components/table/DataTable";
-import SpecialTablePage from "../pages/components/table/SpecialTable";
-import ChartPage from "../pages/components/charts/Charts";
-import EmailTemplate from "../pages/components/email-template/Email";
-import NioIconPage from "../pages/components/crafted-icons/NioIcon";
-import SVGIconPage from "../pages/components/crafted-icons/SvgIcons";
 
 import ProjectCardPage from "../pages/pre-built/projects/ProjectCard";
 import ProjectListPage from "../pages/pre-built/projects/ProjectList";
@@ -79,24 +21,11 @@ import PaymentHistory from "../pages/pre-built/order-invoice/PaymentHistory";
 import ProductCard from "../pages/pre-built/products/ProductCard";
 import ProductList from "../pages/pre-built/products/ProductList";
 import ProductDetails from "../pages/pre-built/products/ProductDetails";
-import InvoiceList from "../pages/pre-built/order-invoice/InvoiceList";
-import InvoiceDetails from "../pages/pre-built/order-invoice/InvoiceDetails";
 import PricingTable from "../pages/earn/Earn";
-import GalleryPreview from "../pages/pre-built/gallery/GalleryCardPreview";
-import ReactToastify from "../pages/components/misc/ReactToastify";
 
 import AppMessages from "../pages/app/messages/Messages";
 import Chat from "../pages/app/chat/ChatContainer";
-import Kanban from "../pages/app/kanban/Kanban";
-import FileManager from "../pages/app/file-manager/FileManager";
 import Inbox from "../pages/app/inbox/Inbox";
-import JsTreePreview from "../pages/components/misc/JsTree";
-import Calender from "../pages/app/calender/Calender";
-import DateTimePicker from "../pages/components/forms/DateTimePicker";
-import QuillPreview from "../pages/components/forms/rich-editor/QuillPreview";
-import TinymcePreview from "../pages/components/forms/rich-editor/TinymcePreview";
-import KnobPreview from "../pages/components/charts/KnobPreview";
-import { FileManagerContextProvider } from "../pages/app/file-manager/FileManagerContext";
 import SwapPage from "../pages/swap/Swap";
 import SellPage from "../pages/marketplace/Sell";
 import { useAccount } from "wagmi";
@@ -145,7 +74,6 @@ const Pages = () => {
         <PrivateRoute path={`${process.env.PUBLIC_URL}/wallets`}>
           <Wallets />
         </PrivateRoute>
-        <PrivateRoute path={`${process.env.PUBLIC_URL}/_blank`} component={Blank}></PrivateRoute>
 
         {/*Pre-built Pages*/}
         <Route exact path={`${process.env.PUBLIC_URL}/project-card`} component={ProjectCardPage}></Route>
@@ -216,9 +144,6 @@ const Pages = () => {
         <PrivateRoute path={`${process.env.PUBLIC_URL}/profile/notifications`}>
           <UserProfileLayout />
         </PrivateRoute>
-        <PrivateRoute path={`${process.env.PUBLIC_URL}/profile/activity`}>
-          <UserProfileLayout />
-        </PrivateRoute>
         <PrivateRoute path={`${process.env.PUBLIC_URL}/profile/settings/`}>
           <UserProfileLayout />
         </PrivateRoute>
@@ -255,96 +180,16 @@ const Pages = () => {
           )}
         ></Route>
         <Route exact path={`${process.env.PUBLIC_URL}/transactions`} component={PaymentHistory}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/invoice-list`} component={InvoiceList}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/invoice-details/:id`} component={InvoiceDetails}></Route>
         <Route exact path={`${process.env.PUBLIC_URL}/earn`} component={PricingTable}></Route>
-
-        <Route exact path={`${process.env.PUBLIC_URL}/image-gallery`} component={GalleryPreview}></Route>
 
         {/*Demo Pages*/}
         <Route exact path={`${process.env.PUBLIC_URL}/pages/terms-policy`} component={Terms}></Route>
         <Route exact path={`${process.env.PUBLIC_URL}/faq`} component={Faq}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/pages/regular-v1`} component={Regularv1}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/pages/regular-v2`} component={Regularv2}></Route>
 
         {/*Application*/}
         <Route exact path={`${process.env.PUBLIC_URL}/app-messages`} component={AppMessages}></Route>
         <Route exact path={`${process.env.PUBLIC_URL}/app-chat`} component={Chat}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/app-calender`} component={Calender}></Route>
-        <Route exact path={`${process.env.PUBLIC_URL}/app-kanban`} component={Kanban}></Route>
         <Route exact path={`${process.env.PUBLIC_URL}/app-inbox`} component={Inbox}></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/files`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/shared`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/starred`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/recovery`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/settings`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/pricing`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
-        <Route
-          exact
-          path={`${process.env.PUBLIC_URL}/app-file-manager/folder/:id`}
-          render={(props) => (
-            <FileManagerContextProvider>
-              <FileManager />
-            </FileManagerContextProvider>
-          )}
-        ></Route>
 
         <Route component={RedirectAs404}></Route>
       </Switch>
